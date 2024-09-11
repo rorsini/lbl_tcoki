ff13ff46688e321d2e9fea6d6d3df3b2800ce8ff
Rob Orsini <rob@tupleshop.com>
2024-09-10 20:56:28 -0700

    added slide deck

--- tcoki.py
+++ tcoki.py
@@ -69,6 +69,7 @@ def profileTcoki(start=1, end=1000):
     tcoki_data['Time'] = tcoki_data['Time'].fillna(tcoki_data['Time'].mean())
     tcoki_data = tcoki_data.drop_duplicates()
     plt.scatter(tcoki_data['Size'], tcoki_data['Time'])
+    #plt.show()
     plt.savefig(f"./images/plot-{end}.png")
 
     return {
